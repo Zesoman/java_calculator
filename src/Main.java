d7190d2b114483c45c8c4c1ae43f9160fafa8c38
Dmytro Borovskyi <midway1555@gmail.com>
2024-02-10 16:29:03 +0200

    implemented calculation logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
+        float[] operands = getInput.operands();
+        String operation = getInput.operation();
+        float result = 0;
 
+        switch (operation) {
+            case "+": {
+                result = Array.getFloat(operands, 0) + Array.getFloat(operands, 1);
+                System.out.println("Result: " + result);
+            }
+            case "-": {
+                result = Array.getFloat(operands, 0) - Array.getFloat(operands, 1);
+                System.out.println("Result: " + result);
+                break;
+            }
+            case "*": {
+                result = Array.getFloat(operands, 0) * Array.getFloat(operands, 1);
+                System.out.println("Result: " + result);
+                break;
+            }
+            case "/": {
+                result = Array.getFloat(operands, 0) / Array.getFloat(operands, 1);
+                System.out.println("Result: " + result);
+                break;
+            }
+            case "%": {
+                result = Array.getFloat(operands, 0) % Array.getFloat(operands, 1);
+                System.out.println("Result: " + result);
+                break;
+            }
+            default: System.out.println("Result: " + result);
+        }
     }
 }
